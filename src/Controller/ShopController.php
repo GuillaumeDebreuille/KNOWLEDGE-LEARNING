16727c8d816c697310442fa8409c23d0ed97f030
Guillaume <guillaume.debreuille@gmail.com>
2026-03-08 12:40:02 +0100

    Import méthod FormationRepository findAll

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\FormationRepository;
+// import FormationRepository to use its methods in ShopController (findAll)
 
 final class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
+    // adding methods of FormationRepository (findAll)
+
     {
+        $formations = $formationRepository->findAll();
+        // ShopController => formationRepository => findAll() => formations (shop)
+        // Data retrieval and storage in $formations (shop)
+
         return $this->render('shop/index.html.twig', [
             'controller_name' => 'ShopController',
+            'formations' => $formations,
+            // allows to use all the data of $formations in the Twig Shop with 'formations'
         ]);
     }
 }
