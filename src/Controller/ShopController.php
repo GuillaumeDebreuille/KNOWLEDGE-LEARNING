dd1142f7510e6a21631db9e6d3dec7f717c6606d
Guillaume <guillaume.debreuille@gmail.com>
2026-04-12 20:04:31 +0200

    mise en forme

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -12,11 +12,12 @@ use App\Repository\LeconRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 // import Request and SessionInterface to handle adding items to the cart in ShopController
+use App\Repository\ProgressRepository;
 
 final class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop')]
-    public function index(FormationRepository $formationRepository, LeconRepository $leconRepository): Response
+    public function index(FormationRepository $formationRepository, LeconRepository $leconRepository, ProgressRepository $progressRepository): Response
     // adding methods of FormationRepository (findAll)
     // adding methods of LeconRepository (findAll)
 
@@ -27,12 +28,40 @@ final class ShopController extends AbstractController
         // ShopController => leconRepository => findAll() => lecons (shop)
         // Data retrieval and storage in $formations and $lecons (shop)
 
+
+
+
+
+        $user = $this->getUser();
+        $progress = $progressRepository->findBy(['user' => $user]);
+        // AccountController => progressRepository => findBy() => progress (account)
+
+        $leconsOwned = [];
+        $formationsOwned = [];
+
+        foreach ($progress as $row) {
+            if ($row -> getLecon()) {
+                $leconsOwned[] = $row -> getLecon() -> getId();
+            }
+            if ($row -> getFormation()) {
+                $formationsOwned[] = $row -> getFormation() -> getId();
+            }
+        }
+        // allows you to create two lists: a list of lessons owned by the user and a list of courses owned by the user.
+        // These lists will allow lessons and training to be displayed in the Twigs thanks to leconsOwned and formationsOwned
+        // The goal is to avoid showing products that are already owned.
+
+
+
+
         return $this->render('shop/index.html.twig', [
             'controller_name' => 'ShopController',
             'formations' => $formations,
             // allows to use all the data of $formations in the Twig Shop with 'formations'
             'lecons' => $lecons,
             // allows to use all the data of $lecons in the Twig Shop with 'lecons'
+            'leconsOwned' => $leconsOwned,
+            'formationsOwned' => $formationsOwned,
         ]);
     }
 
