b1a92c9da2babcb437b89ac18a8765e07c411caa
Guillaume <guillaume.debreuille@gmail.com>
2026-04-15 20:58:59 +0200

    Affichage certification_list

--- src/Controller/CertificationController.php
+++ src/Controller/CertificationController.php
@@ -6,23 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Repository\CertificationRepository;
+
+
+
 final class CertificationController extends AbstractController
 {
-    #[Route('/certification', name: 'app_certification')]
-    public function index(): Response
+    #[Route('/certification_list', name: 'app_certification_list')]
+    public function index(CertificationRepository $certificationRepository): Response
     {
 
+        $user = $this->getUser();
+        $certification = $certificationRepository->findBy(['user' => $user]);
+        $certificationOk = [];
 
-
-
-
-
+        foreach ($certification as $row) {
+            if ($row -> getLecon()) {
+                $certificationOk[] = $row -> getLecon() -> getId();
+            } 
+        }
 
 
 
 
         return $this->render('certification/index.html.twig', [
             'controller_name' => 'CertificationController',
+            'certificationOk' => $certificationOk,
         ]);
+    
     }
 }
